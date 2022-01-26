632453e8223f4e7b9ac722c3c56c84da35ed3cb8
Jai Singh <jai_singh192@optum.com>
2022-01-26 21:19:44 +0530

    initial commit

---
+++ tracer/create.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using static app.trace;// TitanFunctions.DataLayer;
+
+namespace tracer
+{
+    public partial class create : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                //FillTables();
+                FillSolutions(ddlSolutions);
+                //ddlSolutions.Items.AddRange(GetSolutions(urls));
+            }
+
+        }
+        protected void ddlSolutions_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            FillProjects(ddlProjects, ddlSolutions.SelectedValue);
+        }
+        protected void ddlProjects_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            FillActivities(ddlActivities, ddlProjects.SelectedValue);
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            CreateATrace(ddlActivities.SelectedValue, txtIssue.Text, txtSolution.Text, ddlTraceType.SelectedValue);
+        }
+
+    }
+}
